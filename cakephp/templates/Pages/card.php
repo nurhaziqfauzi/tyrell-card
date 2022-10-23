2968ddf0e3099b0ac596d543bedb483a86d3533c
nurhaziqfauzi <hazcom97@gmail.com>
2022-10-23 08:15:33 +0800

    first project commit

---
+++ cakephp/templates/Pages/card.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    
+</html>
+
+<script src="/node_modules/jquery/dist/jquery.js"></script>
+<script type="text/javascript">
+    console.log($);
+</script>
